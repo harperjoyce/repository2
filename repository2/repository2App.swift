2c19a36c8f9bff63c50aa26290def462bdd526fc
hjoyce2024 <hjoyce2024@ihs.immaculateheart.org>
2023-06-26 11:37:48 -0700

    Initial Commit

---
+++ repository2/repository2App.swift
@@ -0,0 +1,17 @@
+//
+//  repository2App.swift
+//  repository2
+//
+//  Created by Harper Joyce on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct repository2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
